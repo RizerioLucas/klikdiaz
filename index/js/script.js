d0a8c3ea077a0c41cccb8d37c98c5b29c3bef1c7
Lucas Rizério <luirizerio@gmail.com>
2024-12-02 10:56:06 -0300

    att edit exemplo

--- index/js/script.js
+++ index/js/script.js
@@ -2,23 +2,28 @@ console.log("Script carregado");
 
 function enviarEmail() {
     console.log("Função enviarEmail chamada");
-    var emailContato = "luirizerio@gmail.com";
-    var subject = "Contato pelo site";
+
+    var emailContato = "diazsalmeida@hotmail.com";  // Email de destino
+    var subject = "Contato pelo site";       // Assunto do email
+
+    // Pega a mensagem que o usuário digitou
     var userMessageElement = document.getElementById("userMessage");
     var body = userMessageElement ? userMessageElement.value : "Esta é uma mensagem de contato gerada pelo site.";
-    var mailtoLink = `mailto:${emailContato}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-    
-    // Mostrar o link no console para verificação
-    console.log("Link do mailto:", mailtoLink);
 
-    window.location.href = mailtoLink;
+    // Cria a URL para o Gmail
+    var gmailURL = `https://mail.google.com/mail/?view=cm&fs=1&to=${encodeURIComponent(emailContato)}&su=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+
+    // Mostra o link no console para verificar
+    console.log("Link do Gmail:", gmailURL);
+
+    // Abre uma nova aba com o link do Gmail
+    window.open(gmailURL, '_blank');
 }
-
 
 function sendWhatsAppMessage() {
     // Número de telefone no formato internacional sem o "+" (código do país + código da cidade + número)
-    var phoneNumber = '5511999999999'; // Exemplo: +55 11 99999-9999
-    var message = 'Olá, gostaria de mais informações!';
+    var phoneNumber = '5511958774472'; // Exemplo: +55 11 99999-9999
+    var message = 'Olá, vim do site e gostaria de mais informações!';
 
     // Codifica a mensagem para ser compatível com URL
     var encodedMessage = encodeURIComponent(message);
